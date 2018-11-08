9180a186aa3cba5037577ae91fcd842f6c657517
y4donts4y <stella.l.griffith@gmail.com>
2018-11-07 23:19:01 -0500

    project code + keys + json

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # liri-node-app
 Liri Bot
+
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+Technologies and libraries include - Spotify, Bands In Town, and OMDB APIs, moment, javascript, json, node, an ignore file, and use of local environments for security.
+
+Available commands users can enter:
+1) concert-this
+2) spotify-this-song
+3) movie-this
+4) do-what-it-says
+
+Concerts
+concert-this allows users to enter the name of an artist and return up to 5 upcoming venues the artist will be playing and the concert dates.
+
+Spotify
+spotify-this-song allows users to enter the name of a song and return the artists, song name, link to song, and album.
+
+Movies
+movie-this allows users to enter the name of a movie and return release year, ratings, production countries, languages, plot, and actors.
+
+Random
+do-what-it-says showcases an ability to read commands from a text file and use it with a command. For the purposes of this project it will return the results for spotify-this-song, song name: "I Want it That Way."
---
+++ liri.js
@@ -0,0 +1,106 @@
+require("dotenv").config();
+
+// Requirements for request, FS, keys, moment, and Spotify
+var request = require("request")
+var fs = require("fs")
+var keys = require("./keys.js");
+var moment = require("moment")
+var Spotify = require('node-spotify-api')
+var spotify = new Spotify(keys.spotify);
+
+// Node arguments
+var command = process.argv[2];
+var name = process.argv[3];
+if (process.argv.length>4){
+    for (i=4; i<process.argv.length; i++){
+        name += " " + process.argv[i]
+    };
+};
+if (command == "concert-this"){
+    concertThis(name);
+}
+else if (command == "spotify-this-song") {
+    spotifyThisSong(name);
+} 
+else if (command == "movie-this") {
+    movieThis(name);
+} 
+else if (command == "do-what-it-says") {
+    doWhatItSays();
+} 
+else {
+    console.log("Please type a valid command: concert-this, spotify-this-song, movie-this, or do-what-it-says")
+};
+
+// concert-this
+function concertThis(enter){
+    if (enter == null) {
+        return console.log("Please enter an artist or band name")
+    }
+    enter = "https://rest.bandsintown.com/artists/" + enter + "/events?app_id=26f7dcaf995dda228b0ecf7ac1a07315"
+    request(enter, function(error, response, body){
+        if (!JSON.parse(body)[0]){
+            return console.log("no results found");
+        }
+        for (i = 0; i<JSON.parse(body).length && i < 5; i++){
+            console.log("\nVenue: " + JSON.parse(body)[i].venue.name)
+            console.log("Venue Location: " + JSON.parse(body)[i].venue.city + ", " + JSON.parse(body)[i].venue.country)
+            console.log("Event Date: " + moment(JSON.parse(body)[i].datetime).format("MM/DD/YYYY"))
+        }
+    })
+};
+
+// spotify-this-song
+function spotifyThisSong(enter){
+    if (enter == null){
+        enter = "The Sign"
+    }
+    spotify.search({type: 'track', query: enter}, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        console.log("\nArtist(s): " + data.tracks.items[0].artists[0].name)
+        console.log("Song: " + data.tracks.items[0].name)
+        console.log("URL: " + data.tracks.items[0].external_urls.spotify)
+        console.log("Album: " + data.tracks.items[0].album.name)
+    })
+};
+
+// movie-this
+function movieThis(enter){
+    if (enter == null){
+        enter = "Mr.Nobody"
+    }
+    request("http://www.omdbapi.com/?t=" + enter + "&y=&plot=short&apikey=ddd1b5c6", function(error, response, body) {        
+        if (!error && response.statusCode === 200) {
+            console.log("\nMovie: " + JSON.parse(body).Title);
+            console.log("Release Year:" + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomato Score: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Production Countries: " + JSON.parse(body).Country);
+            console.log("Languages: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+        }
+    });
+};
+
+// do-what-it-says
+function doWhatItSays(){
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        if (err) {
+          return console.log(err);
+        }      
+        var output = data.split(";");
+        var random = output[Math.floor(Math.random()*output.length)]
+        random = random.split(",")
+        console.log(random)
+        if (random[0] == "concert-this"){
+            concertThis(random[1]);
+        } else if (random[0] == "spotify-this-song") {
+            spotifyThisSong(random[1]);
+        } else if (random[0] == "movie-this") {
+            movieThis(random[1]);
+        }
+      });
+};
